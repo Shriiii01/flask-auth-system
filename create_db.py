3595387af1b8667de80fb933314ea81c302299b2
shrijam12 <shrijambhale8@gmail.com>
2026-01-01 20:04:35 +0530

    m

--- create_db.py
+++
@@ -1,8 +0,0 @@
-from flask_auth import create_app, db
-from flask_auth.models import User, Role
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-    print("Database tables created successfully!") 
